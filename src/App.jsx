816e3b58032b2a5d092f4ba63a36b419ded62e7a
nguyen <tovannguyen12354@gmail.com>
2023-09-23 17:15:12 +0700

    thuc hanh 3 reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,38 @@ import Header from './components/Header'
 import HelloWorld from './components/Helloworld';
 import Main from './components/Main'
 import Navbar from './components/Nav';
+import Age from './components/Age';
 
 
 function App() {
 
+  // age
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState('nguyen')
+  
+  const onIncreateAge = () => {
+    setAge(age + 1);
+  }
+  
+  
+  // header
   const [isLogin, setIsLogin] = useState(false);
 
   const onLoginClick = () => {
     setIsLogin(true);
   }
-
   const onLogoutClick = () => {
     setIsLogin(false)
   }
+
+
   
   return (
   <div>
+    <div className='conatiner'>
+      <button onClick={onIncreateAge}>tăng age lên 1</button>
+      <Age age={age} displayName={name}/>
+    </div>
     <HelloWorld />
     <Header isLogined={isLogin} onLogin={onLoginClick} onLogout={onLogoutClick}/>
     <Navbar />
---
+++ src/components/Age.jsx
@@ -0,0 +1,9 @@
+const Age = (props) => {
+    return (
+        <div>
+            <div> đây là tuổi : {props.age}</div>
+            <div> đây là tên : {props.displayName}</div>
+        </div>
+    )
+}
+export default Age;
