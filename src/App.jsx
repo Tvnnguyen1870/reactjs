991c5f8c97c132a32c6d21856cc7d3981cdabf2e
nguyen <tovannguyen12354@gmail.com>
2023-09-23 16:15:36 +0700

    reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import './App.css'
 
 import Footer from './components/Footer';
-// import Header from './components/Header'
+import Header from './components/Header'
+import HelloWorld from './components/Helloworld';
 import Main from './components/Main'
 
 
 function App() {
 
-  // const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
 
-  // const onLoginClick = () => {
-  //   setIsLogin(true);
-  // }
+  const onLoginClick = () => {
+    setIsLogin(true);
+  }
 
-  // const onLogoutClick = () => {
-  //   setIsLogin(false)
-  // }
+  const onLogoutClick = () => {
+    setIsLogin(false)
+  }
   
   return (
   <div>
-    {/* <Header isLogined={isLogin} onLogin={onLoginClick} onLogout={onLogoutClick}/> */}
+    <HelloWorld />
+    <Header isLogined={isLogin} onLogin={onLoginClick} onLogout={onLogoutClick}/>
     <Main />
     <Footer />
   </div>
